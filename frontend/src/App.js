6d5664282934bd959832fac4b11b8c23628eb2a4
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-29 12:50:41 -0400

    front end shell complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SmartCard from "./Components/SmartCard";
+import Calendar from "./Components/Calendar";
+import Button from '@material-ui/core/Button';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ width: "100%" }}>
+			<div class="grid-container">
+				<div class="cardholder">
+					<SmartCard />
+				</div>
+
+				<div class="schedule">
+					<Calendar />
+				</div>
+
+				<div class="controls">
+					<Button size="small" variant="contained" color="primary">
+						Add New Device
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
