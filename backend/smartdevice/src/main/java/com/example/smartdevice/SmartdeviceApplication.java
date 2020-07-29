dd568128a7036b4237cbcb1de7427763361a335d
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-29 13:25:55 -0400

    new backend

---
+++ backend/smartdevice/src/main/java/com/example/smartdevice/SmartdeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartdevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdeviceApplication.class, args);
+	}
+
+}
